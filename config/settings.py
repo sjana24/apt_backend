2a0f3891d7899fdbf86209421629f120bda251fc
S.Janakan <srijanakan24@gmail.com>
2026-01-03 10:21:16 +0530

    common project setup

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'Authenticate',
     'rest_framework_simplejwt',
-    'Admin'
+    'Admin',
+    'Users',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('Authenticate.urls')),
     path('main/', include('Admin.urls')),
+    path('/', include('Users.urls')),
 ]
